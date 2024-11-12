d9f8cd5500d4bd7de416f3e390e9fc16c7a8c312
Rudra Ushashree <rudarushashree123@gmail.com>
2024-11-12 19:00:06 +0530

    Added changing background color of the page functionality

--- generate_password/src/App.js
+++ generate_password/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState("white");
   return (
-    <div className="App">
-     <h2>Hellooo</h2>
-    </div>
+    <>
+       <div className="full-screen" style={{backgroundColor:color}}>
+        <div className="colorsDiv">
+          <button className="btnColor" onClick={() => setColor("red")} style={{backgroundColor:"red"}}>Red</button>
+          <button className="btnColor" onClick={() => setColor("green")} style={{backgroundColor:"green"}}>Grren</button>
+          <button className="btnColor" onClick={() => setColor("yellow")} style={{backgroundColor:"yellow"}}>Yellow</button>
+          <button className="btnColor" onClick={() => setColor("pink")} style={{backgroundColor:"pink"}}>Pink</button>
+          <button className="btnColor" onClick={() => setColor("orange")} style={{backgroundColor:"orange"}}>Orange</button>
+          <button className="btnColor" onClick={() => setColor("purple")} style={{backgroundColor:"purple"}}>purple</button>
+          <button className="btnColor" onClick={() => setColor("blue")} style={{backgroundColor:"blue"}}>Blue</button>
+        </div>
+      </div>
+    </>
   );
 }
 
